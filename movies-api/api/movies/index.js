9336e4c4a0fa432ff217331961e7fc5238f59f79
unknown <2867459421@qq.com>
2025-01-02 18:16:31 +0800

    Fix the movie/genres port issue

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -5,6 +5,32 @@ import { getUpcomingMovies, fetchFromTMDB } from '../tmdb-api';
 import authenticate from '../../authenticate';
 import Review from './reviewModel';
 const router = express.Router();
+// Get predefined genres
+router.get('/genres', asyncHandler(async (req, res) => {
+    const genres = [
+        { id: 28, name: "Action" },
+        { id: 12, name: "Adventure" },
+        { id: 16, name: "Animation" },
+        { id: 35, name: "Comedy" },
+        { id: 80, name: "Crime" },
+        { id: 99, name: "Documentary" },
+        { id: 18, name: "Drama" },
+        { id: 10751, name: "Family" },
+        { id: 14, name: "Fantasy" },
+        { id: 36, name: "History" },
+        { id: 27, name: "Horror" },
+        { id: 10402, name: "Music" },
+        { id: 9648, name: "Mystery" },
+        { id: 10749, name: "Romance" },
+        { id: 878, name: "Science Fiction" },
+        { id: 10770, name: "TV Movie" },
+        { id: 53, name: "Thriller" },
+        { id: 10752, name: "War" },
+        { id: 37, name: "Western" },
+    ];
+    res.status(200).json({ genres });
+}));
+
 // Get trending movies
 
 router.get('/trending', asyncHandler(async (req, res) => {
@@ -145,30 +171,5 @@ router.get('/:id/reviews', asyncHandler(async (req, res) => {
 }));
 
 
-// Get predefined genres
-router.get('/genres', asyncHandler(async (req, res) => {
-    const genres = [
-        { id: 28, name: "Action" },
-        { id: 12, name: "Adventure" },
-        { id: 16, name: "Animation" },
-        { id: 35, name: "Comedy" },
-        { id: 80, name: "Crime" },
-        { id: 99, name: "Documentary" },
-        { id: 18, name: "Drama" },
-        { id: 10751, name: "Family" },
-        { id: 14, name: "Fantasy" },
-        { id: 36, name: "History" },
-        { id: 27, name: "Horror" },
-        { id: 10402, name: "Music" },
-        { id: 9648, name: "Mystery" },
-        { id: 10749, name: "Romance" },
-        { id: 878, name: "Science Fiction" },
-        { id: 10770, name: "TV Movie" },
-        { id: 53, name: "Thriller" },
-        { id: 10752, name: "War" },
-        { id: 37, name: "Western" },
-    ];
-    res.status(200).json({ genres });
-}));
 
 export default router;
